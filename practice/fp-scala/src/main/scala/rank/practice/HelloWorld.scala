f9128db54be5dd40dd3ba6d2d6939cfd437667bb
williamcs <solobird00@hotmail.com>
2019-08-25 10:25:31 +0800

    first commit.

---
+++ practice/fp-scala/src/main/scala/rank/practice/HelloWorld.scala
@@ -0,0 +1,38 @@
+package rank.practice
+
+/**
+  * A "Hello World" program has been widely adopted as the introductory program for learning how to code.
+  * So, let's start functional programming by writing the same program.
+  *
+  *
+  * Sample Input
+  *
+  * (Nil)
+  * Sample Output
+  *
+  * Hello World
+  * Method Signature
+  *
+  * Number Of Parameters: 0
+  * Parameters: []
+  * Returns: nil
+  * Behavior: Prints "Hello World"
+  * For Hackers Using Clojure
+  *
+  * This will be the outline of your function body. Fill in the blank portion marked by the underscores.
+  *
+  * (___________________________)
+  * For Hackers Using Scala
+  *
+  * This will be the outline of your function body. Fill in the blank portion marked by the underscores.
+  *
+  * def f() = ________________
+  */
+object HelloWorld {
+
+  def f() = println("Hello World")
+
+  def main(args: Array[String]): Unit = {
+    f()
+  }
+}
